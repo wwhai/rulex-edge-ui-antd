cf65f69772439f5f85cfa3e7d8f39a4dfcb9857b
wwhai <cnwwhai@gmail.com>
2022-08-20 23:31:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# RULEX UI
+
+该项目是全新的RULEX边缘网关框架专属UI，主要用来给开发者提供统一便利的前端体验。
+
+## 环境搭建
+
+Install `node_modules`:
+
+```bash
+npm install
+```
+
+or
+
+```bash
+yarn
+```
+
+### Start project
+
+```bash
+npm start
+```
+
+### Build project
+
+```bash
+npm run build
+```
+
+### Check code style
+
+```bash
+npm run lint
+```
+
+You can also use script to auto fix some lint error:
+
+```bash
+npm run lint:fix
+```
+
+### Test code
+
+```bash
+npm test
+```
+
+## More
+
+该项目由RULEX社区维护。
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,44 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import { useIntl } from '@umijs/max';
+
+const Footer: React.FC = () => {
+  const intl = useIntl();
+  const defaultMessage = intl.formatMessage({
+    id: 'app.copyright.produced',
+    defaultMessage: 'RULEX Team',
+  });
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'RULEX',
+          title: 'RULEX',
+          href: 'https://pro.ant.design',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: true,
+        },
+        {
+          key: 'RULEX',
+          title: 'RULEX',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+
+export default Footer;
